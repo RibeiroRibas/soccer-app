0e1724acc8cec1c142ba16d6ecac294fa6d9e173
Ribeiro <rafael.ribeiro@evoluum.com.br>
2023-05-20 20:38:05 -0300

    renomeia atributos

--- lib/shared/themes/theme_colors.dart
+++ lib/shared/themes/theme_colors.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 class ThemeColors {
   static const MaterialColor division = Colors.grey;
   static const Color principalPosition = Colors.orange;
-  static const Color secondaryPosition = Colors.white;
-  static const Color tertiaryPosition = Colors.pinkAccent;
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
   static const Color primary = Color.fromRGBO(0, 255, 66, 1);
   static const cardColor = Color.fromRGBO(34, 34, 50, 1);
   static const backgroundColor = Color.fromRGBO(24, 24, 40, 1);
