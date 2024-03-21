bfb7f933f24d9adaa9750483eaf0cf3899a7b2d7
Rafael Ribeiro <rafael.ribeiro@evoluum.com.br>
2024-03-21 18:15:40 -0300

    implement automatic values for match settings view

--- lib/modules/new_match/view/match_settings/match_settings_view.dart
+++ lib/modules/new_match/view/match_settings/match_settings_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:mobx/mobx.dart';
 import 'package:team_draw/model/match_settings.dart';
 import 'package:team_draw/model/player.dart';
 import 'package:team_draw/modules/new_match/view/match_settings/widget/match_duration_time_widget.dart';
@@ -12,7 +13,7 @@ import 'package:team_draw/ui/section/tittle_section.dart';
 import 'widget/select_one_option_widget.dart';
 
 class MatchSettingsView extends StatefulWidget {
-  final List<Player> selectedPlayers;
+  final Map<Player, bool> selectedPlayers;
   final MatchSettings matchSettings;
 
   const MatchSettingsView(
@@ -24,81 +25,99 @@ class MatchSettingsView extends StatefulWidget {
 
 class _MatchSettingsViewState extends State<MatchSettingsView> {
   MatchSettingsViewModel viewModel = MatchSettingsViewModel();
+  late ReactionDisposer _disposerNumberOfTeams;
 
   @override
   void initState() {
     super.initState();
-    viewModel.changeSide(widget.matchSettings.hasChangeSide ?? false);
-    viewModel.changeReserve(widget.matchSettings.hasReservePlayer ?? false);
-    viewModel.drawNewTeams(widget.matchSettings.isDrawNewTeams ?? false);
+    _init();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _disposerNumberOfTeams = autorun((_) {
+      widget.matchSettings.numberOfTeams = viewModel.numberOfTeams;
+      widget.matchSettings.numberOfStartingPlayers =
+          viewModel.numberOfPlayersByTeam;
+    });
+  }
+
+  Future<void> _init() async {
+    await viewModel.init(widget.selectedPlayers.values, widget.matchSettings);
   }
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          const Padding(
-            padding: EdgeInsets.only(top: 16.0, bottom: 16.0),
-            child: TittleSection(tittle: matchSettings),
-          ),
-          MatchDurationTimeWidget(
-            durationHr: widget.matchSettings.durationHr,
-            durationMin: widget.matchSettings.durationMin,
-            onDurationHrChange: (value) =>
-                widget.matchSettings.durationHr = value,
-            onDurationMinChange: (value) =>
-                widget.matchSettings.durationMin = value,
-          ),
-          const Divider(color: ThemeColors.division),
-          SelectBoxWidget(
-            value: widget.matchSettings.numberOfTeams,
-            onValueChange: (value) =>
-                widget.matchSettings.numberOfTeams = value,
-            values: ListHelper.getListOfTeams(),
-            description: numberOfTeams,
-            labelText: quantity,
-          ),
-          const Divider(color: ThemeColors.division),
-          SelectBoxWidget(
-            value: widget.matchSettings.numberOfStartingPlayers,
-            onValueChange: (value) =>
-                widget.matchSettings.numberOfStartingPlayers = value,
-            values: ListHelper.getListOfPlayers(),
-            description: numberOfPlayersByTeam,
-            labelText: quantity,
-          ),
-          const Divider(color: ThemeColors.division),
-          Observer(
-            builder: (_) => SelectOneOptionWidget(
-                question: hasChangeSideQuestion,
-                value: viewModel.hasChangeSide!,
-                onValueSelected: (value) {
-                  viewModel.changeSide(value);
-                  widget.matchSettings.hasChangeSide = value;
-                }),
-          ),
-          const Divider(color: ThemeColors.division),
-          SelectBoxWidget(
-            value: widget.matchSettings.timeToChangePlayer,
-            onValueChange: (value) =>
-                widget.matchSettings.timeToChangePlayer = value,
-            values: ListHelper.getListOfMinutes(),
-            description: changePlayerEvery,
-            labelText: minute,
-          ),
-          const Divider(color: ThemeColors.division),
-          Observer(
-            builder: (_) => SelectOneOptionWidget(
-                question: newTeamsQuestion,
-                value: viewModel.isDrawNewTeams!,
-                onValueSelected: (value) {
-                  viewModel.drawNewTeams(value);
-                  widget.matchSettings.isDrawNewTeams = value;
-                }),
-          ),
-          const Text(newTeamsSubTittle),
-        ],
+    return Observer(
+      builder: (_) => SingleChildScrollView(
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(top: 16.0, bottom: 16.0),
+              child: TittleSection(tittle: matchSettings),
+            ),
+            MatchDurationTimeWidget(
+              durationHr: widget.matchSettings.durationHr,
+              durationMin: widget.matchSettings.durationMin,
+              onDurationHrChange: (value) =>
+                  widget.matchSettings.durationHr = value,
+              onDurationMinChange: (value) =>
+                  widget.matchSettings.durationMin = value,
+            ),
+            const Divider(color: ThemeColors.division),
+            SelectBoxWidget(
+              value: viewModel.numberOfTeams,
+              onValueChange: (value) {
+                viewModel.changeNumberOfTeams(value);
+              },
+              values: ListHelper.getListOfPossibleTeams(
+                  viewModel.getTotalPlayers()),
+              description: numberOfTeams,
+              labelText: quantity,
+            ),
+            const Divider(color: ThemeColors.division),
+            SelectBoxWidget(
+              value: viewModel.numberOfPlayersByTeam,
+              onValueChange: (value) =>
+                  widget.matchSettings.numberOfStartingPlayers = value,
+              values: ListHelper.getListOfTotalPlayersPossibleByTeam(
+                  viewModel.numberOfPlayersByTeam),
+              description: numberOfPlayersByTeam,
+              labelText: quantity,
+            ),
+            const Divider(color: ThemeColors.division),
+            Observer(
+              builder: (_) => SelectOneOptionWidget(
+                  question: hasChangeSideQuestion,
+                  value: viewModel.hasChangeSide!,
+                  onValueSelected: (value) {
+                    viewModel.changeSide(value);
+                    widget.matchSettings.hasChangeSide = value;
+                  }),
+            ),
+            const Divider(color: ThemeColors.division),
+            SelectBoxWidget(
+              value: widget.matchSettings.timeToChangePlayer,
+              onValueChange: (value) =>
+                  widget.matchSettings.timeToChangePlayer = value,
+              values: ListHelper.getListOfMinutes(),
+              description: changePlayerEvery,
+              labelText: minute,
+            ),
+            const Divider(color: ThemeColors.division),
+            Observer(
+              builder: (_) => SelectOneOptionWidget(
+                  question: newTeamsQuestion,
+                  value: viewModel.isDrawNewTeams!,
+                  onValueSelected: (value) {
+                    viewModel.drawNewTeams(value);
+                    widget.matchSettings.isDrawNewTeams = value;
+                  }),
+            ),
+            const Text(newTeamsSubTittle),
+          ],
+        ),
       ),
     );
   }
@@ -106,5 +125,6 @@ class _MatchSettingsViewState extends State<MatchSettingsView> {
   @override
   void dispose() {
     super.dispose();
+    _disposerNumberOfTeams();
   }
 }
--- lib/modules/new_match/view_model/match_settings_view_model.g.dart
+++ lib/modules/new_match/view_model/match_settings_view_model.g.dart
@@ -9,6 +9,22 @@ part of 'match_settings_view_model.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
 
 mixin _$MatchSettingsViewModel on MatchSettingsViewModelBase, Store {
+  Computed<int>? _$numberTotalOfPossibleTeamsComputed;
+
+  @override
+  int get numberTotalOfPossibleTeams =>
+      (_$numberTotalOfPossibleTeamsComputed ??= Computed<int>(
+              () => super.numberTotalOfPossibleTeams,
+              name: 'MatchSettingsViewModelBase.numberTotalOfPossibleTeams'))
+          .value;
+  Computed<int>? _$numberOfPlayersByTeamComputed;
+
+  @override
+  int get numberOfPlayersByTeam => (_$numberOfPlayersByTeamComputed ??=
+          Computed<int>(() => super.numberOfPlayersByTeam,
+              name: 'MatchSettingsViewModelBase.numberOfPlayersByTeam'))
+      .value;
+
   late final _$hasChangeSideAtom =
       Atom(name: 'MatchSettingsViewModelBase.hasChangeSide', context: context);
 
@@ -22,22 +38,6 @@ mixin _$MatchSettingsViewModel on MatchSettingsViewModelBase, Store {
   set hasChangeSide(bool? value) {
     _$hasChangeSideAtom.reportWrite(value, super.hasChangeSide, () {
       super.hasChangeSide = value;
-    });
-  }
-
-  late final _$hasReservePlayerAtom = Atom(
-      name: 'MatchSettingsViewModelBase.hasReservePlayer', context: context);
-
-  @override
-  bool? get hasReservePlayer {
-    _$hasReservePlayerAtom.reportRead();
-    return super.hasReservePlayer;
-  }
-
-  @override
-  set hasReservePlayer(bool? value) {
-    _$hasReservePlayerAtom.reportWrite(value, super.hasReservePlayer, () {
-      super.hasReservePlayer = value;
     });
   }
 
@@ -57,6 +57,48 @@ mixin _$MatchSettingsViewModel on MatchSettingsViewModelBase, Store {
     });
   }
 
+  late final _$numberOfTeamsAtom =
+      Atom(name: 'MatchSettingsViewModelBase.numberOfTeams', context: context);
+
+  @override
+  int? get numberOfTeams {
+    _$numberOfTeamsAtom.reportRead();
+    return super.numberOfTeams;
+  }
+
+  @override
+  set numberOfTeams(int? value) {
+    _$numberOfTeamsAtom.reportWrite(value, super.numberOfTeams, () {
+      super.numberOfTeams = value;
+    });
+  }
+
+  late final _$arePlayersSelectedAtom = Atom(
+      name: 'MatchSettingsViewModelBase.arePlayersSelected', context: context);
+
+  @override
+  Iterable<bool> get arePlayersSelected {
+    _$arePlayersSelectedAtom.reportRead();
+    return super.arePlayersSelected;
+  }
+
+  @override
+  set arePlayersSelected(Iterable<bool> value) {
+    _$arePlayersSelectedAtom.reportWrite(value, super.arePlayersSelected, () {
+      super.arePlayersSelected = value;
+    });
+  }
+
+  late final _$initAsyncAction =
+      AsyncAction('MatchSettingsViewModelBase.init', context: context);
+
+  @override
+  Future<void> init(
+      Iterable<bool> arePlayersSelected, MatchSettings matchSettings) {
+    return _$initAsyncAction
+        .run(() => super.init(arePlayersSelected, matchSettings));
+  }
+
   late final _$MatchSettingsViewModelBaseActionController =
       ActionController(name: 'MatchSettingsViewModelBase', context: context);
 
@@ -66,17 +108,6 @@ mixin _$MatchSettingsViewModel on MatchSettingsViewModelBase, Store {
         .startAction(name: 'MatchSettingsViewModelBase.changeSide');
     try {
       return super.changeSide(changeSide);
-    } finally {
-      _$MatchSettingsViewModelBaseActionController.endAction(_$actionInfo);
-    }
-  }
-
-  @override
-  void changeReserve(bool changeReserve) {
-    final _$actionInfo = _$MatchSettingsViewModelBaseActionController
-        .startAction(name: 'MatchSettingsViewModelBase.changeReserve');
-    try {
-      return super.changeReserve(changeReserve);
     } finally {
       _$MatchSettingsViewModelBaseActionController.endAction(_$actionInfo);
     }
@@ -94,11 +125,25 @@ mixin _$MatchSettingsViewModel on MatchSettingsViewModelBase, Store {
   }
 
   @override
+  void changeNumberOfTeams(int numberOfTeams) {
+    final _$actionInfo = _$MatchSettingsViewModelBaseActionController
+        .startAction(name: 'MatchSettingsViewModelBase.changeNumberOfTeams');
+    try {
+      return super.changeNumberOfTeams(numberOfTeams);
+    } finally {
+      _$MatchSettingsViewModelBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
   String toString() {
     return '''
 hasChangeSide: ${hasChangeSide},
-hasReservePlayer: ${hasReservePlayer},
-isDrawNewTeams: ${isDrawNewTeams}
+isDrawNewTeams: ${isDrawNewTeams},
+numberOfTeams: ${numberOfTeams},
+arePlayersSelected: ${arePlayersSelected},
+numberTotalOfPossibleTeams: ${numberTotalOfPossibleTeams},
+numberOfPlayersByTeam: ${numberOfPlayersByTeam}
     ''';
   }
 }
