8a6a6746c5ff9cfb6205eea75925ef8fe0e71d7c
Rafael Ribeiro <96504657+RibeiroRibas@users.noreply.github.com>
2024-04-21 20:24:34 -0300

    Add info to edit team and shield (#30)
    * organizando pacotes
    
    * feature select theme
    
    * native splash screen
    
    * refactor packages
    
    * change RouterOutlet to PageView
    
    * add new player characteristics
    
    * add new player characteristics
    
    * add shield
    
    * add info to edit team name and shield
    
    ---------
    
    Co-authored-by: Ribeiro <rafael.ribeiro@evoluum.com.br>

--- lib/data/shield_data.dart
+++ lib/data/shield_data.dart
@@ -7,5 +7,15 @@ List<String> getAllShields = [
   "$imageInitialPath/logo-figueirense-256.png",
   "$imageInitialPath/logo-palmeiras-256.png",
   "$imageInitialPath/logo-dortmund-256.png",
-  "$imageInitialPath/logo-red-bull-bragantino-256.png"
+  "$imageInitialPath/logo-red-bull-bragantino-256.png",
+  "$imageInitialPath/logo-atletico-mineiro-256.png",
+  "$imageInitialPath/logo-atletico-paranaense-256.png",
+  "$imageInitialPath/logo-bahia-256.png",
+  "$imageInitialPath/logo-botafogo-256.png",
+  "$imageInitialPath/logo-cruzeiro-256.png",
+  "$imageInitialPath/logo-fluminense-256.png",
+  "$imageInitialPath/logo-santos-256.png",
+  "$imageInitialPath/logo-sao-paulo-256.png",
+  "$imageInitialPath/logo-vasco-da-gama-256.png",
+  "$imageInitialPath/logo-vitoria-256.png",
 ];
